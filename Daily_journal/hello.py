23c20f085ea8b874e6c148ee78f0dd7826980385
Kamakshi478 <kamakshikumari478@gmail.com>
2026-08-15 15:19:08 +0530

    Add files via upload

---
+++ Daily_journal/hello.py
@@ -0,0 +1,6 @@
+# dictionary 
+# Adding and modifying elements in a dictionary
+user={"name":"John", "age":30, "city":"New York"}
+user["city"]='Los Angeles'  # Modifying an existing element
+print(user)
+
