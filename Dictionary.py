65d4d71d8dda4b158254721f9122f79d0113db9c
Kamakshi478 <kamakshikumari478@gmail.com>
2026-09-15 21:48:23 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,5 @@
+student={'name': 'Alice', 'age': 20, 'grade': 'A'}
+print(student.keys()) # Returns all the keys in the dictionary 
+print(student.values()) # Returns all the values in the dictionary 
+print(student.items()) # Returns all the key-value pairs in the dictionary  
+
