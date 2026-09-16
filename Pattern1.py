b834d6b9eaca3253c31b35d2cf31cc6b60cd2815
Kamakshi478 <kamakshikumari478@gmail.com>
2026-09-16 21:02:59 +0530

    Add files via upload

---
+++ Pattern1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def pattern1(self, N):
+        for i in range(N):
+            for j in range(N):
+                print(" * ", end=" ")
+            print()
+s= Solution()
+s.pattern1(3)
